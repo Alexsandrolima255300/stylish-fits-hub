9ff1412181d836f53a6661915c61fb472ced2e5e
Alexsandrolima255300 <alexsandrolima255300@gmail.com>
2026-08-13 14:18:36 -0300

    style: load PetViva CRM gallery refinements

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const style=document.createElement("link");
+style.rel="stylesheet";
+style.href="/petviva-overrides.css";
+document.head.appendChild(style);
+
 createRoot(document.getElementById("root")!).render(<App />);
